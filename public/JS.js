54c06e6dd4f630cdc93c1ab511db9a6ed968c49e
Nixgit2000 <fourwaycoordination@gmail.com>
2023-02-06 11:50:50 -0500

    added js, tables to organize testimonials

---
+++ public/JS.js
@@ -0,0 +1,40 @@
+
+var one = document.getElementById("one");
+
+one.addEventListener("mouseover", function function1() {
+    one.style.scale = 1.05;
+    one.style.fontSize = "3vh"
+})
+one.addEventListener("mouseout", function function1() {
+    one.style.scale = 1.0;
+})
+
+var two = document.getElementById("two");
+
+two.addEventListener("mouseover", function function1() {
+    two.style.scale = 1.05;
+    two.style.fontSize = "3vh"
+})
+two.addEventListener("mouseout", function function1() {
+    two.style.scale = 1.0;
+})
+
+var three = document.getElementById("three");
+
+three.addEventListener("mouseover", function function1() {
+    three.style.scale = 1.05;
+    three.style.fontSize = "3vh"
+})
+three.addEventListener("mouseout", function function1() {
+    three.style.scale = 1.0;
+})
+
+var four = document.getElementById("four");
+
+four.addEventListener("mouseover", function function1() {
+    four.style.scale = 1.05;
+    four.style.fontSize = "3vh"
+})
+four.addEventListener("mouseout", function function1() {
+    four.style.scale = 1.0;
+})
